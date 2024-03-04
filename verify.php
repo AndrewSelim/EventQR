e94b79da0352d90490f5b1475698c7b6d8f41300
AndrewSelim <162083650+AndrewSelim@users.noreply.github.com>
2024-03-04 07:00:20 +0200

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php
 // Valid QR codes array
-$validQRCodes = array("12345678", "18887773");
+$validQRCodes = array("1", "5556", "12345678");
 
 // Read JSON input from the client-side
 $data = json_decode(file_get_contents('php://input'), true);
@@ -8,10 +8,19 @@ $data = json_decode(file_get_contents('php://input'), true);
 // Extract the QR code from the input data
 $qrCode = $data['qrCode'];
 
-// Check if the QR code is valid
-$isValid = in_array($qrCode, $validQRCodes);
+// Check if the QR code is in the list of valid QR codes
+$isScannedBefore = false;
+if (in_array($qrCode, $validQRCodes)) {
+    // If QR code is valid, check if it has been scanned before
+    if (isset($_SESSION['scannedQRCodes']) && in_array($qrCode, $_SESSION['scannedQRCodes'])) {
+        $isScannedBefore = true;
+    } else {
+        // If QR code is valid and not scanned before, mark it as scanned
+        $_SESSION['scannedQRCodes'][] = $qrCode;
+    }
+}
 
 // Return verification result as JSON
 header('Content-Type: application/json');
-echo json_encode(['isValid' => $isValid]);
+echo json_encode(['isValid' => in_array($qrCode, $validQRCodes), 'isScannedBefore' => $isScannedBefore]);
 ?>
